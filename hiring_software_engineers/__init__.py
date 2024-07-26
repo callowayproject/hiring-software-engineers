3bfd39a313b83b922e869cf5027aaa57b34cb405
Corey Oordt <coreyoordt@gmail.com>
2024-07-26 15:01:59 -0500

    Initial commit

---
+++ hiring_software_engineers/__init__.py
@@ -0,0 +1,3 @@
+"""Top-level package for hiring-software-engineers."""
+
+__version__: str = "0.1.0"
---
+++ tests/test_hiring_software_engineers.py
@@ -0,0 +1,4 @@
+"""Tests for `hiring_software_engineers` package."""
+
+import pytest
+from hiring_software_engineers import hiring_software_engineers
